5b87cebd07dddc6e6ba66e8f47489c1be4e39d0b
HaydenWalton12 <70973137+HaydenWalton12@users.noreply.github.com>
2023-10-31 15:49:04 +0000

    Continue when I get home

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -14,53 +14,71 @@ struct Node
 
 };
 
-//Insert new node 
-void insert_back(Node** node, int data)
+
+//Inserts new node at front of list, requires current head as this new node will become the new current head.
+Node* insert_front(struct Node** current_head, int new_data)
 {
-	Node* new_node = new Node(data);
 
-	if ((*node)->next == nullptr)
+	//Allocate New Node
+	Node* new_head = new Node(new_data);
+
+	//Make next of new node as head and previous nullptr.
+	//Why? New node is will be infront, making this node the new head
+	new_head->next = (*current_head);
+	new_head->previous = nullptr;
+
+	//Make head nodes previous point to new node
+	if ((*current_head) != nullptr)
 	{
-		new_node->previous = (*node);
-
+		(*current_head)->previous = new_head;
 	}
-
-
+	//Replace address of current head to point to new head, this is required to ensure if we want to use the push function again.
+	(*current_head) = new_head;
+	return new_head;
 }
 
-//Insert new node with data in front of list
-void insert_front(Node** current_node, int data)
+Node* insert_between(struct Node* previous_node, int new_data)
 {
-	//Creates New Node
-	Node* new_node = new Node(data);
-	
-	//Places current node infront of new node
-	new_node->next = (*current_node);
-
-	//If list is not empty
-	if ((*current_node) != nullptr)
+	//Check if node is nullptr, we cant insert if the node we want to insert with doesnt exist
+	if (previous_node == nullptr)
 	{
-		(*current_node)->previous = new_node;
+		std::cout << "Given previous node cannot be nullptr\n";
+		return nullptr;
 	}
 
-	//Point head to new node
-	(*current_node) = new_node;
+	//Allocate New Node
+	Node* new_node = new Node(new_data);
+
+	//New node will now be behind previous_nodes next
+	//This places the next pointer now behind new_node
+	new_node->next = previous_node->next;
+	
+	//Previous node now needs to store the new_node in next
+	//As this is the node now infront
+	previous_node->next = new_node;
+
+	//To complete this node being inbetween, new nodes previous
+	//points to previous_node
+	new_node->previous = previous_node;
+
+	//Change previous of new_nodes next node
+	//We do this in conditional to ensure there is a node infront of new_node.
+	if (new_node->next != nullptr)
+	{
+		new_node->next->previous = new_node;
+	}
+
+	return new_node;
+
 }
 //Contains Reference to Linking/Walking through heap
 int main()
 {
+	Node* head = new Node(1);
+	Node* node_1 = insert_front(&head, 2);
+	Node* node_2 = insert_front(&head, 3);
+	Node* node_3 = insert_between(node_2, 5);
 
-
-	// create a pointer to the head node
-	Node* head = nullptr;
-
-	// create a pointer to end node
-	Node* end = nullptr;
-	// call the insert_at_front function four times
-	insert_front(&head, 1);
-	insert_front(&head, 2);
-	insert_front(&head, 3);
-	insert_front(&head, 4);
 
 }
 
